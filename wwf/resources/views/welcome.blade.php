8487d33ffe52165daf80499cc668293d5dbd404e
Gáspár Imre <ifj.gaspar.imre@gmail.com>
2021-11-04 15:09:41 +0100

    Multi level menu system
    created ,and it's available for logged in users

--- wwf/resources/views/welcome.blade.php
+++ wwf/resources/views/welcome.blade.php
@@ -156,13 +156,70 @@
                         <ul class="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-b-0 sm:border-r md:border-b-0 pb-6 pt-6 lg:pt-3">
                             <h3 class="font-bold text-xl text-white text-bold">For Users</h3>
                             @auth
-                            <li>
-                                <a href="#" class="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white">Upload Photo</a>
 
-                            </li>
-                            <li>
-                                <a href="#" class="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white">Join us!</a>
-                            </li>
+
+
+    <div class="group inline-block">
+        <button
+            class="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white"     >
+            <span class="pr-1 font-semibold flex-1">Manage</span>
+            <span>
+      <svg
+          class="fill-current h-4 w-4 transform group-hover:-rotate-180
+        transition duration-150 ease-in-out"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+      >
+        <path
+            d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+        />
+      </svg>
+    </span>
+        </button>
+        <ul
+            class="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute
+  transition duration-150 ease-in-out origin-top min-w-32"
+        >
+
+            <li class="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white">Photos</li>
+            <li class="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white">Articles</li>
+
+
+          <svg
+              class="fill-current h-4 w-4
+            transition duration-150 ease-in-out"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+          >
+            <path
+                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+            />
+          </svg>
+
+
+                    </div>
+
+    <style>
+        /* since nested groupes are not supported we have to use
+           regular css for the nested dropdowns
+        */
+        li>ul                 { transform: translatex(100%) scale(0) }
+        li:hover>ul           { transform: translatex(101%) scale(1) }
+        li > button svg       { transform: rotate(-90deg) }
+        li:hover > button svg { transform: rotate(-270deg) }
+
+        /* Below styles fake what can be achieved with the tailwind config
+           you need to add the group-hover variant to scale and define your custom
+           min width style.
+             See https://codesandbox.io/s/tailwindcss-multilevel-dropdown-y91j7?file=/index.html
+             for implementation with config file
+        */
+        .group:hover .group-hover\:scale-100 { transform: scale(1) }
+        .group:hover .group-hover\:-rotate-180 { transform: rotate(180deg) }
+        .scale-0 { transform: scale(0) }
+        .min-w-32 { min-width: 8rem }
+    </style>
+
                             @else
                                 <li>
                                     <a href="#" class="block p-3 hover:bg-blue-600 text-gray-300 hover:text-white">Please Log In</a>
