00870194fb679c31ad8da5384ef365b7fa232885
Gáspár Imre <ifj.gaspar.imre@gmail.com>
2021-11-04 15:10:26 +0100

    removed one space

--- wwf/resources/views/dashboard.blade.php
+++ wwf/resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                     <div class="h-64 bg-cover lg:rounded-lg lg:h-full" ><img src="{{url('/images/iceland.jpg')}}"></div>
                 </div>
                 <div class="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
-                    <h2 class="text-3xl text-gray-800 font-bold">Welcome  <span class="text-red-600">Logged in</span> user</h2>
+                    <h2 class="text-3xl text-gray-800 font-bold">Welcome <span class="text-red-600">Logged in</span> user</h2>
                     <p class="mt-4 text-gray-600">This website made by Imre Gáspár, it's just a HomeWork, so errors/issues can occur. I hope you will enjoy this site.</p>
                 </div>
             </div>
