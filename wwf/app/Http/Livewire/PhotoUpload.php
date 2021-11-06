15fd6468875f7c6134854a5ed318cdd9a9d072c3
Gáspár Imre <ifj.gaspar.imre@gmail.com>
2021-11-06 14:44:54 +0100

    Update PhotoUpload.php

--- wwf/app/Http/Livewire/PhotoUpload.php
+++ wwf/app/Http/Livewire/PhotoUpload.php
@@ -4,7 +4,9 @@ namespace App\Http\Livewire;
 use Illuminate\Support\Facades\File;
 use Livewire\Component;
 use Livewire\WithFileUploads;
-
+use Illuminate\Http\Request;
+use Illuminate\Http\ValidationException;
+use Illuminate\Support\Facades\DB;
 
 class PhotoUpload extends Component
 {
@@ -13,17 +15,21 @@ class PhotoUpload extends Component
     public $photo;
     public $name;
     public $fileNames = [];
-
+    public $titleNames = [];
+    public $title;
     public function submit()
     {
         $this->validate([
             'photo' => 'image|max:12288',
         ]);
         $name = $this->photo->getClientOriginalName();
-//        $this->photo->storePubliclyAs('photos', $name);
-        $this->photo->store('photos');
-        dd($this->name);
-//        session()->flash('message', __('Photo uploaded '));
+        $this->photo->storePubliclyAs('photos', $name);
+
+        DB::table('image_gallery')->insert([
+            'title'=>$this->title,
+            'image'=>$this->photo,
+        ]);
+        session()->flash('message', __('Photo uploaded '));
     }
 
     public function mount()
