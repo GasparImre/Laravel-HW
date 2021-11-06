3774cb7e00513935e18e417e567dfa7c1f200921
Gáspár Imre <ifj.gaspar.imre@gmail.com>
2021-11-06 11:47:54 +0100

    Update User.php
    HasRoles lines added

--- wwf/app/Models/User.php
+++ wwf/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -16,7 +17,7 @@ class User extends Authenticatable
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
-    use TwoFactorAuthenticatable;
+    use TwoFactorAuthenticatable,HasRoles;
 
     /**
      * The attributes that are mass assignable.
