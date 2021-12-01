b984c4d03edca23031a2fb9bb8098919a6ec2075
Gáspár Imre <ifj.gaspar.imre@gmail.com>
2021-12-01 19:35:34 +0100

    Update ContactSeeder.php

--- wwf/database/seeders/ContactSeeder.php
+++ wwf/database/seeders/ContactSeeder.php
@@ -14,11 +14,11 @@ class ContactSeeder extends Seeder
      */
     public function run()
     {
-//        DB::table('contact')->insert([
-//        'fullname' => Str::random(30),
-//        'email'=>Str::random(10).'@yahoo.com',
-//        'message'=>Str::random(150),
-//    ]);
+        DB::table('contact')->insert([
+        'fullname' => Str::random(30),
+        'email'=>Str::random(10).'@yahoo.com',
+        'message'=>Str::random(150),
+    ]);
         ContactModel::factory()
             ->count(30)
             ->create();
