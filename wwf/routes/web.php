51633739b2de06e10728d207f4e63298aa4f8dc9
Gáspár Imre <ifj.gaspar.imre@gmail.com>
2021-12-01 19:36:12 +0100

    Update web.php

--- wwf/routes/web.php
+++ wwf/routes/web.php
@@ -22,16 +22,17 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
+//Photo
 Route::get('/image-gallery',[\App\Http\Controllers\ImageGalleryController::class, 'index'])->name('image-gallery');
 Route::post('/image-gallery',[\App\Http\Controllers\ImageGalleryController::class, 'upload'])->name('image-upload');
 Route::delete('/image-gallery/{id}',[\App\Http\Controllers\ImageGalleryController::class, 'destroy'])->name('image-destroy');
-
+//Contact page
 Route::get('/contactus',[\App\Http\Controllers\ContactUsController::class,'create'])->name('contactus');
 Route::post('/contactus',[\App\Http\Controllers\ContactUsController::class, 'store']);
+//Video page
 Route::get('/videos',[\App\Http\Controllers\Controller::class, 'play'])->name('play');
 
-
+Auth::routes();
 //Articles
 Route::get('/articles',[ArticleController::class,'index'])->name('articles.index');
 Route::get('/articles/create',[ArticleController::class,'create'])->name('articles.create');
@@ -42,10 +43,20 @@ Route::put('/articles/{article}',[ArticleController::class,'update'])->name('art
 Route::delete('/articles/{article}',[ArticleController::class,'destroy'])->name('articles.destroy');
 
 //Users
-Route::get('/users',[UserController::class,'index'])->name('users.index');
-Route::get('/users/create',[UserController::class,'create'])->name('users.create');
-Route::post('/users',[UserController::class,'store'])->name('users.store');
-Route::get('/users/{user}',[UserController::class,'show'])->name('users.show');
-Route::get('/users/{user}/edit',[UserController::class,'edit'])->name('users.edit');
-Route::put('/users/{user}',[UserController::class,'update'])->name('users.update');
-Route::delete('/users/{user}',[UserController::class,'destroy'])->name('users.destroy');
+//Route::group(['middleware'=> 'auth2'],function() {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+//});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
