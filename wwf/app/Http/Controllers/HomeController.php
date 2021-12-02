f1188dae1aacdc520aa6b995ecc2b233dd3ca818
Gáspár Imre <ifj.gaspar.imre@gmail.com>
2021-12-02 19:31:22 +0100

    Update HomeController.php

--- wwf/app/Http/Controllers/HomeController.php
+++ wwf/app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,20 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+//        Role::create(['name'=>'admin']);
+//        Role::create(['name'=>'guest']);
+//
+//       Permission::create(['name'=>'user']);
+//       auth()->user()->givePermissionTo('user');
+//        auth()->user()->assignRole('guest');
+//       return auth()->user()->roles;
+//       return auth()->user()->permissions;
+// Permission adása a super usernek
+//        $user = Auth::user();
+//        $user = Users::find($req->id);
+//        $user = $req->input('Users');
+//        $user = $req->user();
+//        dd($user);
+//        $user->givePermissionTo('Super User');
     }
 }
